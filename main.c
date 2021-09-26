2ca2a116f736a943e90c35d6a8de36cdc6fbae2f
norangmoran <87406167+norangmoran@users.noreply.github.com>
2021-09-27 02:15:28 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void setup(){
   Wire.begin();
   
   Serial.begin(9600);
-  DoorLock.attach(11);
+  DoorLock.attach(13); //서보모터 Data핀
 }
 
 void loop(){
@@ -64,4 +64,59 @@ void LockSys(char seri){ //도어락 잠금 시스템
           LCDPRINT("Hello, Visitor!","Enter password..");
           break;
     }
+}
+
+void RoomMode(int RM){ //조명모드 선택
+  switch(RM) {
+    case 1://아침 기상모드
+      ONOFFCont(1);
+      RGB1(242,0,0);
+      //주백색-활동적인 에너지를 주는 색. 주방, 몸 움직이는 일.
+      break;
+    case 2://일상 생활모드
+      ONOFFCont(0);
+      RGB1(235,255,255);
+//      RGB2(235,255,255);
+      //백색-상쾌함. 긴장감. 책읽기, 사무공간.
+      break;
+    case 3://오후 휴식모드
+      ONOFFCont(1);
+      RGB1(223,140,50);
+ //     RGB2(223,140,50);
+      //전구색-백열전구와 비슷. 안정감을 주는 휴식공간.
+      break;
+    case 4://저녁 취침모드
+      ONOFFCont(2);
+//      RGB2(223,140,50);
+      break;
+    default://외출 소등
+      ONOFFCont(6);
+      break;
+  }
+}
+
+void DoorLock(char LOCK){ //도어락 잠금열림
+  switch(LOCK){
+    case 'Y':
+      myservo.write(0); //angle 0도. 열림
+      break;
+    default:
+      myservo.write(90); //angle 90도. 닫힘
+      break;
+  }
+}
+void ONOFFCont(int cont) { //조명 OnOff컨트롤. 시프트레지스터 제어.
+  digitalWrite(latch,LOW);
+  shiftOut(data,clock,MSBFIRST,cont);
+  digitalWrite(latch,HIGH);
+}
+void RGB1(int R,int G,int B){ //거실창가 조명
+  analogWrite(red1,R);
+  analogWrite(green1,G);
+  analogWrite(blue1,B);
+}
+void RGB2(int R,int G,int B){ //거실내부 조명
+ analogWrite(red2,R);
+ analogWrite(green2,G);
+ analogWrite(blue2,B);
 }
